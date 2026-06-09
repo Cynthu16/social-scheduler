7e13b936f944046443837e3c3036fdbdfddee5d4
Cynthu16 <cynthiadsouza1601@gmail.com>
2026-06-09 14:45:21 +0530

    sidebar second commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Layout from "./components/Home/Layout";
+import DashBoard from "./pages/DashBoard";
+import Accounts from "./pages/Accounts";
+import Scheduler from "./pages/Scheduler";
+import AIComposer from "./pages/AIComposer";
 
 export default function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<DashBoard />} />
+          <Route path="/accounts" element={<Accounts />} />
+          <Route path="/schedule" element={<Scheduler />} />
+          <Route path="/ai-composer" element={<AIComposer />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
---
+++ client/src/pages/Scheduler.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Scheduler() {
+  return <div></div>;
+}
+
+export default Scheduler;
