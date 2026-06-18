5fcd7d071da814b9e2fa24acd6df4c0a431b6054
Cynthu16 <cynthiadsouza1601@gmail.com>
2026-06-18 17:55:59 +0530

    Accounts mounted with PlatformPickerModal and AccountLists

--- client/src/pages/Accounts.tsx
+++ client/src/pages/Accounts.tsx
@@ -1,7 +1,74 @@
-import React from "react";
+import { PlusIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+import { dummyAccountsData, PLATFORMS } from "../assets/assets";
+import AccountList from "../components/Home/AccountList";
+import PlatformPickerModal from "../components/Home/PlatformPickerModal";
 
 function Accounts() {
-  return <div></div>;
+  const [accounts, setAccounts] = useState<any[]>([]);
+  const [connecting, setConnecting] = useState<string | null>(null);
+  const [showPlatformPicker, setShowPlatformPicker] = useState(false);
+
+  const fetchAccounts = async (
+    isSync = false,
+    platform?: string | null,
+    successMsg?: string,
+  ) => {
+    setAccounts(dummyAccountsData);
+    console.log(isSync, platform, successMsg);
+  };
+  useEffect(() => {
+    fetchAccounts();
+  }, []);
+
+  const handleConnect = async (platformId: string) => {
+    setConnecting(platformId);
+    setTimeout(() => {
+      setConnecting(null);
+      setAccounts((prev) => [...prev, dummyAccountsData[0]]);
+      setShowPlatformPicker(false);
+    }, 1000);
+  };
+
+  const handleDisconnect = async (accountId: string) => {
+    setAccounts(accounts.filter((a) => a.id !== accountId));
+  };
+
+  const connectedIDs = accounts.map((accounts) => accounts.platform);
+
+
+  return (
+    <div className="space-y-8 max-w-4xl">
+      {/* Header */}
+      <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4 text-sm">
+        <div>
+          <h2 className="text-xl text-slate-900">Connected Accounts</h2>
+          <p className="text-slate-500 text-sm mt-0.5">
+            {accounts.length} of {PLATFORMS.length} platforms connected{" "}
+          </p>
+        </div>
+        <button
+          onClick={() => setShowPlatformPicker(true)}
+          className="flex items-center gap-2 px-2 py-2.5 bg-red-500 hover:bg-red-600 text-white rounded-full font-medium transition-all w-full sm:w-auto justify-center"
+        >
+          <PlusIcon className="size-4" /> Connect Account
+        </button>
+      </div>
+      {/* Platform picker mode */}
+
+      {showPlatformPicker && (
+        <PlatformPickerModal
+          connectedIds={connectedIDs}
+          connecting={connecting}
+          onClose={() => setShowPlatformPicker(false)}
+          onConnect={handleConnect}
+        />
+      )}
+
+      {/* List of connected Accounts */}
+      <AccountList accounts={accounts} onDisconnect={handleDisconnect} />
+    </div>
+  );
 }
 
 export default Accounts;
